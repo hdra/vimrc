3f1440cfbbff901a598f1e2288b5f2a521292d51
Hendra <hendra2392@gmail.com>
2022-08-17 17:06:17 +0700

    Remove go-specific test runner

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -4,8 +4,3 @@ go.setup({
   dap_debug_gui = false,
   dap_debug_vt = false
 })
-
-
-vim.cmd [[ nnoremap <leader>tn :GoTest -n<cr>]]
-vim.cmd [[ nnoremap <leader>tf :GoTest -f<cr>]]
-vim.cmd [[ nnoremap <leader>tb :GoDebug -t<cr>]]
